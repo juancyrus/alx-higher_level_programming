ec4cc696aec6a53788a36a3089686ddb9a2584d3
juancyrus <mragbadinoahkekeli@gmail.com>
2023-12-05 11:46:52 +0000

    sliding

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0 :
+        return my_list
+    if idx >= len(my_list) :
+        return my_list 
+    return my_list[idx]
---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    reversed = mylist[::-1]
+    for numbers in reversed:
+        print("{:d}".format(numbers))

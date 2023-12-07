4f2186486f2073887c114b441451aca98330ae15
juancyrus <mragbadinoahkekeli@gmail.com>
2023-12-07 11:21:37 +0000

    breathe

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-     new_string = ''
+    new_string = ''
     for char in my_string:
         if char.lower() != 'c':
             new_string += char
-        if char.upper() != 'C':
     return new_string
